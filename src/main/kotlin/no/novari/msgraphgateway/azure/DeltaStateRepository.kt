513d18d599e55886aa9e52bff442106b3d8a3e44
chadaj <agata.chadaj@novari.no>
2026-01-29 08:52:32 +0100

    init

--- src/main/kotlin/no/novari/msgraphgateway/azure/DeltaStateRepository.kt
+++
@@ -1,8 +0,0 @@
-package no.novari.msgraphgateway.azure
-
-import org.springframework.data.repository.CrudRepository
-
-interface DeltaStateRepository: CrudRepository<DeltaState, String>
-{
-    fun findDeltaStateByName(name: String): DeltaState?
-}
