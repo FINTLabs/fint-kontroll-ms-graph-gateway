6386a649bfe6e8de6849d7aa989a8523f71ffadb
chadaj <agata.chadaj@novari.no>
2026-03-14 17:47:14 +0100

    revert change

--- src/main/kotlin/no/novari/msgraphgateway/config/ConfigUser.kt
+++ src/main/kotlin/no/novari/msgraphgateway/config/ConfigUser.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component
 import java.util.LinkedHashSet
 
 @Component
-@ConfigurationProperties(prefix = "ms-graph.user")
+@ConfigurationProperties(prefix = "fint.kontroll.ms-graph-gateway.user")
 class ConfigUser {
     companion object {
         @JvmStatic
