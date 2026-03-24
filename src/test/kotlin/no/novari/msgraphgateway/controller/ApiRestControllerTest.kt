6702efd96e2dab63106f7e443874fbc806689028
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2026-03-24 16:01:09 +0100

    FKS-1564 #comment Test made for fullimport.

---
+++ src/test/kotlin/no/novari/msgraphgateway/controller/ApiRestControllerTest.kt
@@ -0,0 +1,38 @@
+package no.novari.msgraphgateway.controller
+
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import no.novari.msgraphgateway.config.ConfigUser
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ApiRestControllerTest {
+    @Test
+    fun getUserWithGroups() {
+    }
+
+    @Test
+    fun testTriggerUserFullSync() {
+        runTest {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+
+    @Test
+    fun triggerUserDeltaSync() {
+    }
+
+    @BeforeEach
+    fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    @AfterEach
+    fun tearDown() {
+        TODO("Not yet implemented")
+    }
+}
