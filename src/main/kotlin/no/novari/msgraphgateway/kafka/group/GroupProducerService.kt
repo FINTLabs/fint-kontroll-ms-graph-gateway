7e1dd3700e2b8b3e24b251ebc5bbd51efcf34b13
OlaBrustad <ola.brustad@gmail.com>
2026-07-01 12:14:16 +0200

    added traceId and status for Groups

--- src/main/kotlin/no/novari/msgraphgateway/kafka/group/GroupProducerService.kt
+++ src/main/kotlin/no/novari/msgraphgateway/kafka/group/GroupProducerService.kt
@@ -8,6 +8,7 @@ import no.novari.kafka.topic.configuration.EntityCleanupFrequency
 import no.novari.kafka.topic.configuration.EntityTopicConfiguration
 import no.novari.kafka.topic.name.EntityTopicNameParameters
 import no.novari.kafka.topic.name.TopicNamePrefixParameters
+import no.novari.msgraphgateway.entra.EntraStatus
 import no.novari.msgraphgateway.entra.group.EntraGroup
 import no.novari.msgraphgateway.entra.group.EntraGroupPayload
 import org.slf4j.LoggerFactory
@@ -52,28 +53,55 @@ class GroupProducerService(
         )
     }
 
-    fun publish(entraGroup: EntraGroup) {
-        log.debug("Publishing group to Kafka: {}", entraGroup.displayName)
+    fun publish(
+        entraGroup: EntraGroup,
+        status: EntraStatus = entraGroup.status ?: EntraStatus.CREATED,
+    ) {
+        log.debug(
+            "Publishing group to Kafka: objectId={}, displayName={}, traceId={}, status={}",
+            entraGroup.objectId,
+            entraGroup.displayName,
+            entraGroup.traceId,
+            status,
+        )
 
         entraGroupTemplate.send(
             ParameterizedProducerRecord
                 .builder<EntraGroupPayload>()
                 .topicNameParameters(entityTopicNameParameters)
                 .key(entraGroup.objectId.toString())
-                .value(entraGroup.toPayload())
+                .value(entraGroup.toPayload(status))
                 .build(),
         )
     }
 
-    fun publishDeletedGroup(groupId: String) {
-        log.debug("Publishing deleted group tombstone to Kafka: {}", groupId)
+    fun publishDeletedGroup(
+        groupId: String,
+        resourceGroupId: Long? = null,
+        traceId: String? = null,
+    ) {
+        log.debug(
+            "Publishing deleted group to Kafka: objectId={}, resourceGroupId={}, traceId={}",
+            groupId,
+            resourceGroupId,
+            traceId,
+        )
+
+        val payload =
+            EntraGroupPayload(
+                objectId = groupId,
+                displayName = null,
+                resourceGroupId = resourceGroupId,
+                traceId = traceId,
+                status = EntraStatus.DELETED,
+            )
 
         entraGroupTemplate.send(
             ParameterizedProducerRecord
                 .builder<EntraGroupPayload>()
                 .topicNameParameters(entityTopicNameParameters)
                 .key(groupId)
-                .value(null)
+                .value(payload)
                 .build(),
         )
     }
